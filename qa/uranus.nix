2d241a3c01d3c9e354acfe9156667efcbed2df1d
Lucas Rasmussen <lucasnethaj@gmail.com>
2024-08-15 15:42:31 +0200

    make wheel a trusted user

--- qa/uranus.nix
+++ qa/uranus.nix
@@ -40,4 +40,9 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  nix.settings.trusted-users = [
+      "root"
+      "@wheel"
+  ];
 }
